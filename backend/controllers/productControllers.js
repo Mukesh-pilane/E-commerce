5ba8cfbaac7fc35faa93fa1dac9d2a0cc8fb0df4
Mukesh Pilane <mukeshpilane@gmail.com>
2023-03-01 01:51:36 +0530

    first commit

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,15 @@
 const Product = require("../models/productModels")
 
+//create product - admin
+
+exports.createProducts = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success:true,
+    product
+  })
+}
+
 exports.getAllProducts = (req, res)=>{
   return res.status(200).json({message: 'fine'})
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {getAllProducts} = require("../controllers/productControllers")
+const {getAllProducts, createProducts} = require("../controllers/productControllers")
 const router = express.Router()
 
 
 router.route("/products").get(getAllProducts)
+router.route("/products/new").post(createProducts)
 
 
 module.exports = router
